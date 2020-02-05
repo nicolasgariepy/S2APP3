a0556fccd907140a437d8180d58906960deaa261
nicolasgariepy <nicolaslegariepy@gmail.com>
2020-02-04 23:07:07 -0500

    Ajout surchage << et --/++

--- Vecteur.h
+++ Vecteur.h
@@ -7,15 +7,22 @@ class Vecteur {
 public:
 	Vecteur();
 	~Vecteur();
-	T& operator [] (int i) { return(data[i]); }
+	int getTaille() { return taille; }
+	int getCapacite() { return capacite; }
+	int getCourant() { return courant; }
+	T & operator [] (int i) { return(data[i]); }
 	void operator += (T ajout);
-	int taille;
-	int capacite;
+	Vecteur & operator ++ ();
+	Vecteur & operator -- ();
+	ostream & afficher(int index, ostream & s);
+	friend ostream & operator <<(ostream & s, const Vecteur<T>& vct);
 
 
 private:
 	T* data;
-
+	int taille;
+	int capacite;
+	int courant;
 	void doubler();
 };
 
@@ -23,6 +30,7 @@ template <typename T>
 Vecteur<T>::Vecteur() {
 	capacite = 1;
 	taille = 0;
+	courant = 0;
 	data = new T[capacite];
 	(*this)[0] = 0;
 }
@@ -53,4 +61,40 @@ void Vecteur<T>::doubler() {
 	}
 	
 	data = buffer;
+}
+
+template <typename T>
+Vecteur<T>& Vecteur<T>::operator ++ () {
+	if (courant < taille)
+		++courant;
+	return *this;
+}
+
+template <typename T>
+Vecteur<T>&  Vecteur<T>::operator -- () {
+	if (courant > 0)
+		--courant;
+	return *this;
+}
+
+template <typename T>
+ostream & Vecteur<T>::afficher(int index, ostream & s) {
+	if (T == DonneesTest) {
+		s << "Type test: " << T.typeTest << endl
+			<< "Adresse switches: " << T.registreSW << endl
+			<< "Retour switches: " << dec << T.retourSW << "(" << hex << T.retourSW << ")" << endl
+			<< "Etat switches: " << dec << T.etatSW << "(" << hex << T.etatSW << ")" << endl
+			<< "Adresse leds: " << T.registreLD << endl
+			<< "Valeur leds: " << dec << T.valeurLD << "(" << hex << T.valeurLD << ")" << endl
+			<< "Etat leds: " << dec << T.etatLD << "(" << hex << T.etatLD << ")" << endl
+	}
+	return s;
+}
+
+template <typename T>
+ostream & operator <<(ostream & s, const Vecteur<T>& vct) {
+	for (int i = 0; i <= taille; i++) {
+		vct.afficher(i, &s);
+	}
+	return s;
 }
